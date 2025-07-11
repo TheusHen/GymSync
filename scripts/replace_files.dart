7934cc0b078cf9489b9e527965e5e60b74133bac
I'm Matheus <codelong@proton.me>
2025-07-11 17:52:23 -0300

    V1.2.4

--- scripts/replace_files.dart
+++ scripts/replace_files.dart
@@ -9,6 +9,18 @@ void main() async {
     {
       'source': '.github/replace/build.gradle.kts',
       'target': 'apps/mobile_app/android/app/build.gradle.kts',
+    },  
+    {
+      'source': '.github/replace/drawable/ic_notification.png',
+      'target': 'apps/mobile_app/android/app/src/main/res/drawable/ic_notification.png',
+    },
+    {
+      'source': '.github/replace/drawable/ic_pause.png',
+      'target': 'apps/mobile_app/android/app/src/main/res/drawable/ic_pause.png',
+    },
+    {
+      'source': '.github/replace/drawable/ic_stop.png',
+      'target': 'apps/mobile_app/android/app/src/main/res/drawable/ic_stop.png',
     },
   ];
 
@@ -21,8 +33,10 @@ void main() async {
       continue;
     }
 
-    final content = await sourceFile.readAsString();
-    await targetFile.writeAsString(content);
+    await targetFile.parent.create(recursive: true);
+
+    final content = await sourceFile.readAsBytes();
+    await targetFile.writeAsBytes(content);
 
     print('Replaced: ${file['target']}');
   }
